82b6949a1ac24cc1699d109abd78dc6cf6c8de4b
Luis Fernández <luis.brfernandez@gmail.com>
2017-09-14 03:37:18 -0500

    add gravatar img, improve css, add more tests

--- test/integration/recipes_test.rb
+++ test/integration/recipes_test.rb
@@ -3,9 +3,9 @@ require 'test_helper'
 class RecipesTest < ActionDispatch::IntegrationTest
 
   def setup
-    @user = Chef.create!(name:"TestChef", email:"thisisatest@email.com", password_digest:"asdasfasdas")
-    @recipe = @user.recipes.create!(name:"Test recipe", description: "Add a lot of test to your app!")
-    @recipe2 = @user.recipes.create!(name:"Test recipe 2", description: "Add a lot of test to your app! v2")
+    @chef = Chef.create!(name:"TestChef", email:"thisisatest@email.com", password_digest:"asdasfasdas")
+    @recipe = @chef.recipes.create!(name:"Test recipe", description: "Add a lot of test to your app!")
+    @recipe2 = @chef.recipes.create!(name:"Test recipe 2", description: "Add a lot of test to your app! v2")
   end
 
   test "should get recipes index" do
@@ -16,8 +16,8 @@ class RecipesTest < ActionDispatch::IntegrationTest
   test "should get recipes listing" do
     get recipes_path
     assert_template 'recipes/index'
-    assert_match @recipe.name, response.body
-    assert_match @recipe2.name, response.body
+    assert_select "a[href=?]", recipe_path(@recipe), text: @recipe.name
+    assert_select "a[href=?]", recipe_path(@recipe2), text: @recipe2.name
   end
 
   test "should get new recipe page" do
@@ -33,6 +33,14 @@ class RecipesTest < ActionDispatch::IntegrationTest
   test "should get edit recipe page" do
     get edit_recipe_path(@recipe)
     assert_response :success
+  end
+
+  test "should get recipes show" do
+    get recipe_path(@recipe)
+    assert_template 'recipes/show'
+    assert_match @recipe.name, response.body
+    assert_match @recipe.description, response.body
+    assert_match @chef.name, response.body
   end
 
 end
