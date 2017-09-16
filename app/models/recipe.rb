c405f1586245b8ed500bc5e5e96f87c77a0b2e37
Luis Fernández <luis.brfernandez@gmail.com>
2017-09-16 16:56:08 -0500

    Change max lenght of recipe description from 500 to 1000 characters

--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -2,7 +2,7 @@ class Recipe < ApplicationRecord
   belongs_to :chef
   validates_associated :chef
   validates :name, presence: true, length: { minimum: 2 }
-  validates :description, presence: true, length: { in: 5..500 }
+  validates :description, presence: true, length: { in: 5..1000 }
   validates :chef_id, presence: true
   default_scope -> { order(updated_at: :desc)}
 end
